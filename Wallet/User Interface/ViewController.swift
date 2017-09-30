6df463de31a71a1411663df9cf8f8df5bef139dd
Boris Yurkevich <boris.yurkevich@manything.com>
2017-09-30 22:44:43 +0100

    Now displaying user balance on screen.

--- Wallet/User Interface/ViewController.swift
+++ Wallet/User Interface/ViewController.swift
@@ -10,13 +10,20 @@ import UIKit
 
 final class ViewController: UIViewController {
     
-    private let service = EuropeanCentralBankService()
-    private let parser = Parser()
-    var coreData: PersistanceModel? = nil
-    var user: User?
-    
     @IBAction func tapRefresh(_ sender: Any) {
         actionRefresh()
+    }
+    
+    private let service = EuropeanCentralBankService()
+    private let parser = Parser()
+    private var coreData: PersistanceModel? = nil
+    private var user: User?
+    private var carouselViewController: CarouselViewController?
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "carousel" {
+            carouselViewController = segue.destination as? CarouselViewController
+        }
     }
     
     override func viewDidLoad() {
@@ -25,7 +32,7 @@ final class ViewController: UIViewController {
         coreData = PersistanceModel()
         user = coreData?.fetchUser()
         if user != nil {
-            print("user found")
+            displayUserBalance()
         } else {
             print("user not found")
         }
@@ -38,6 +45,22 @@ final class ViewController: UIViewController {
         } else {
             print("XML Parser Error: \(parser.parsingError!)")
         }
+    }
+    
+    private func displayUserBalance() {
+        for account in user!.accounts!.allObjects as! [Account] {
+            switch account.currencyType! {
+                case CurrencyType.usd.rawValue:
+                    carouselViewController?.leftLabel.text = "$\(account.balance)"
+                case CurrencyType.eur.rawValue:
+                    carouselViewController?.rightLabel.text = "€\(account.balance)"
+                case CurrencyType.gbp.rawValue:
+                    carouselViewController?.mainLabel.text = "£\(account.balance)"
+                default:
+                    break
+            }
+        }
+        
     }
 
 
